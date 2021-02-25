9bdb381b0401d73b5ffc46961e96cdff628c5753
Anthony Leon <Acidknights89@gmail.com>
2021-02-25 01:17:47 -0500

    Add logic to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.find_by_id(params[:id])
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id 
+            redirect_to @user 
+        else
+            @error = @user.errors.full_messages.to_sentence
+            render new 
+        end
+    end
+    
 end
