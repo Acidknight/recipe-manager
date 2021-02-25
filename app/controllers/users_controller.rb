ad3a3419e82279e87c6fca5ab1cd2b90866fa8dd
Anthony Leon <Acidknights89@gmail.com>
2021-02-25 01:55:50 -0500

    Add show logic to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,12 @@ class UsersController < ApplicationController
         @user = User.find_by_id(params[:id])
     end
 
+    def show
+        redirect_if_not_logged_in
+        @user = User.find_by_id(params[:id])
+        redirect_to '/' if !@user
+    end
+
     private
 
     def user_params
