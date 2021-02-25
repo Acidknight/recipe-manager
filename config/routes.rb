0eca81c87e151d13b1564e1fc628be630073f77f
Anthony Leon <Acidknights89@gmail.com>
2021-02-25 01:27:45 -0500

    Add routes for signup and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :comments
   resources :recipes
   resources :users
+  root 'sessions#home'
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
