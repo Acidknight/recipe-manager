be7c6eb15f43a316c4eeffc58f637b1805c73fc4
Anthony Leon <Acidknights89@gmail.com>
2021-02-25 00:00:39 -0500

    Add relationship logic to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+    belongs_to :user 
+    has_many :comments
+    has_many :user_comments, through: :comments, source: :user 
+
+    validates :title, :description, :instructions, :preparation_time, :rating, presence: true
 end
