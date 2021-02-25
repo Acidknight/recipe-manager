a912db298aa78938db4c11b87f03be6462b6d992
Anthony Leon <Acidknights89@gmail.com>
2021-02-25 01:25:13 -0500

    Add logic to create a new session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+    def home
+        @user = User.find_by_id(params[:id])
+    end
+
+    def create
+        @user = User.find_by_id(params[:id])
+        if @username && user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else
+            redirect_to '/login'
+        end
+    end
+        
 end
