8aa23c02c013151cc49154e47af310ea04720356
Anthony Leon <Acidknights89@gmail.com>
2021-02-24 23:27:13 -0500

    Generate table for recipes

---
+++ db/migrate/20210225042644_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :description
+      t.text :instructions
+      t.integer :preparation_time
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
