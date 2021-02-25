bae1bd26a515815bba7475e2c326767b54a9f7a8
Anthony Leon <Acidknights89@gmail.com>
2021-02-24 23:57:11 -0500

    Add relationship logic to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    has_many :recipes
+    has_many :comments
+    has_many :recipe_comments, through: :recipes, source: :comments
+
+    validates :email, presence: true
+    validates :username, presence: true, uniqueness:true
 end
